b22baa7a5658e73803988bf29bd39da10b078301
Traveler <lakshyadwivedi2005@gmail.com>
2026-05-11 17:30:11 +0000

    comparision and datatype summery

---
+++ Basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+//console.log(null == 0)
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+// console.log(undefined == 0)
+
+/******STRICT-CHECK ( === )********/
+//it is totaly different from (==), in this(===) it strictly check the condition.It also check dataType
+console.log("2" == 2)//true
+console.log("2" === 2)//false
